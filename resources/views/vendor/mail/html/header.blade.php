682bae2350e28801b466376aa3179b69396a0821
KeweKiwi <kevinwilliamfaith@gmail.com>
2025-06-09 10:39:29 +0700

    design email sent

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,9 @@
+@props(['url'])
+
+<tr>
+<td class="header">
+    <a href="{{ $url }}" style="display: inline-block;">
+        <img src="https://i.postimg.cc/YStY0F4r/veravia-logo.png" alt="Veravia Logo" style="height: 100px; max-height: 120px; width: auto;">
+    </a>
+</td>
+</tr>
